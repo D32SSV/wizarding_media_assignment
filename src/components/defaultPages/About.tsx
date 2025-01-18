34cbc31bac5949da1070146516b7e1243f041a25
D32SSV <131158288+D32SSV@users.noreply.github.com>
2025-01-18 10:29:26 +0530

    remove React import

--- src/components/defaultPages/About.tsx
+++ src/components/defaultPages/About.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const About = () => {
   return <p>This is About Page</p>;
 };
--- src/components/defaultPages/notfound.tsx
+++ src/components/defaultPages/notfound.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Notfound = () => {
   return <p className="text-9xl">404 Not found</p>;
 };
